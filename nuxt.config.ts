d166486e54bab07adf3faacdeea945bf4f3e38d8
José Filho <jc.bsfilho@gmail.com>
2025-07-01 15:18:10 -0300

    refactor: update nuxt configuration and enhance build scripts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
-})
+  nitro: {
+    preset: "azion",
+    serveStatic: true,
+    compressPublicAssets: true,
+  },
+  compatibilityDate: "2024-03-30",
+  devtools: { enabled: true },
+});
